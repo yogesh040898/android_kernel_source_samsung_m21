7231f3e92964b0afa1e116f96bb670eb2cd73938
yogesh040898 <kingyogesh04@gmail.com>
2021-04-15 23:53:51 -0700

    Fixing : Issue caused by Header Files
    Signed-off-by: yogesh040898 <kingyogesh04@gmail.com>

--- README.md
+++
@@ -1,2 +0,0 @@
-# android_kernel_source_samsung_m21
-Android kernel Source for Samsung M21
--- ged.sh
+++
@@ -1,6 +0,0 @@
-gedit arch/arm64/kernel/module.lds
-gedit arch/x86/Makefile
-gedit drivers/block/zram/zram_drv.c
-gedit drivers/md/dm-table.c
-gedit scripts/Makefile.build
-
